bcf94bf0aa3287a76cc3e4337050843039ec677f
baberrehman <baberrehman26@gmail.com>
2021-05-22 20:22:50 +0800

    functions

--- 3-functions.hs
+++ 3-functions.hs
@@ -31,3 +31,91 @@ second (_, y, _) = y
 
 third :: (a, b, c) -> c
 third (_, _, z) = z
+
+head' :: [a] -> a
+head' []    = error "Can't call head on empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell []       = "The list is empty"
+tell (x:[])   = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_)  = "The list is long. First two elements are: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' []     = 0
+length' (_:xs) = 1 + (length' xs)
+
+sum' :: (Num a) => [a] -> a
+sum' []     = 0
+sum' (x:xs) = x + (sum' xs)
+
+capital :: String -> String
+capital "" = "Empty String!"
+capital all@(x:_) = "First letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+    | bmi <= 25.0 = "You're normal"
+    | bmi <= 18.5 = "You're underweight"
+    | bmi <= 30.0 = "You're fat"
+    | otherwise   = "You're a whale"
+
+bmiTell' :: (RealFloat a) => a -> a -> String
+bmiTell' weight height
+    | weight / height ^ 2 <= 25.0 = "You're normal"
+    | weight / height ^ 2 <= 18.5 = "You're underweight"
+    | weight / height ^ 2 <= 30.0 = "You're fat"
+    | otherwise   = "You're a whale"
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a > b     = a
+    | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b      = GT
+    | a == b     = EQ
+    | otherwise  = LT
+
+bmiTell'' :: (RealFloat a) => a -> a -> String
+bmiTell'' weight height
+    | bmi <= 25.0 = "You're normal"
+    | bmi <= 18.5 = "You're underweight"
+    | bmi <= 30.0 = "You're fat"
+    | otherwise   = "You're a whale"
+    where bmi = weight / height ^ 2
+
+bmiTell''' :: (RealFloat a) => a -> a -> String
+bmiTell''' weight height
+    | bmi <= skinny = "You're underweight"
+    | bmi <= normal = "You're normal"
+    | bmi <= fat = "You're fat"
+    | otherwise   = "You're a whale"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea  = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ case xs of [] -> "empty"
+                                               [x] -> "a singleton list"
+                                               xs -> "a longer list"
+
+describeList' :: [a] -> String
+describeList' xs = "The list is " ++ what xs
+    where what []   = "empty"
+          what [x]  = "a singleton list"
+          what xs = "a longer list"
