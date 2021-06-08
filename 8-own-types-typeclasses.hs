57ada0741547ba4b3a0ce0daa8f32e718171321e
baberrehman <baberrehman26@gmail.com>
2021-06-09 01:40:46 +0800

    own types and typeclasses

--- 8-own-types-typeclasses.hs
+++ 8-own-types-typeclasses.hs
@@ -2,7 +2,7 @@
 import qualified Data.Map as Map  
 
 
-data Bool = False | True
+-- data Bool = False | True
 
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
@@ -111,3 +111,31 @@ lockers = Map.fromList
     ,(110,(Taken,"99292"))  
     ]
 
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys      = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq, Ord)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node y left right)
+  | x == y = Node y left right
+  | x < y  = Node y (treeInsert x left) right
+  | x > y  = Node y left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem _ EmptyTree = False
+treeElem x (Node y left right)
+  | x == y = True
+  | x < y  = treeElem x left
+  | x > y  = treeElem x right
